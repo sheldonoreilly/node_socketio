d13887a432f131554c9ea1b4e74e1900f7f568da
Sheldon O'Reilly <sheldon.oreilly@gmail.com>
2018-08-30 23:46:28 -0230

    introducted socket.io

--- server/server.js
+++ server/server.js
@@ -1,13 +1,24 @@
 const path = require("path");
-
 const express = require("express");
-const app = express();
+const http = require("http");
+const socketIO = require("socket.io");
 
 const port = process.env.PORT || 3000;
-
 const publicPath = path.join(__dirname, "../public");
-app.use(express.static(publicPath));
 
-app.listen(port, () => {
+const app = express();
+
+const server = http.createServer(app);
+const io = socketIO(server);
+
+io.on("connection", socket => {
+	console.log("New User connected");
+	socket.on("disconnect", () => {
+		console.log("Client is disconnected");
+	});
+});
+
+app.use(express.static(publicPath));
+server.listen(port, () => {
 	console.log(`The server is up at ${port}.`);
 });
